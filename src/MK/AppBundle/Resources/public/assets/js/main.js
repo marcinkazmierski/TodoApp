8a6591792785a73d9564715ae725036d4e9ad83b
Marcin Kaźmierski <marcin.ryszard.kazmierski@gmail.com>
2016-10-16 18:40:03 +0200

    Refactoring js.

--- src/MK/AppBundle/Resources/public/assets/js/main.js
+++ src/MK/AppBundle/Resources/public/assets/js/main.js
@@ -1,3 +1,15 @@
 jQuery(function () {
 
 });
+
+
+function createAlert(message, type) {
+    if (type == undefined) {
+        type = 'success';
+    }
+    var html = '<div class="container"><div class="row margin-top"><div class="col-sm-12"><div class="alert alert-' + type + '" role="alert">';
+    html += '<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>';
+    html += message;
+    html += '</div></div></div></div>';
+    jQuery('.flash-messages').html(html);
+}
