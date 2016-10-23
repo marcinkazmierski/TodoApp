e1e30d1269d51f82a79c655fd1e77abfc834ee84
Marcin Kaźmierski <marcin.ryszard.kazmierski@gmail.com>
2016-10-23 10:30:37 +0200

    Delete cateory confirm.

--- src/MK/AppBundle/Resources/public/assets/js/main.js
+++ src/MK/AppBundle/Resources/public/assets/js/main.js
@@ -2,6 +2,7 @@ jQuery(function () {
 
 });
 
+var currentCategoryBox = false;
 
 function createAlert(message, type) {
     if (type == undefined) {
@@ -12,4 +13,131 @@ function createAlert(message, type) {
     html += message;
     html += '</div></div></div></div>';
     jQuery('.flash-messages').html(html);
+}
+
+function loadCategories() {
+    loadTasksBox('.home-categories');
+}
+
+
+function loadTasksBox(el) {
+    var action = jQuery(el).attr('data-action');
+    var $this = jQuery(el);
+    jQuery.ajax({
+        url: action,
+        method: 'GET'
+    }).done(function (data) {
+        if (data.status == 1) {
+            $this.html(data.content);
+            bindAddNewTask($this);
+        }
+    });
+    currentCategoryBox = false;
+}
+
+function bindAddNewTask(el) {
+    jQuery(el).find('.add-new-task, .edit-task').click(function () {
+
+        var action = jQuery(this).attr('data-action');
+        var category = jQuery(this).attr('data-category');
+        var $this = jQuery(this);
+        $this.button('loading');
+
+
+        currentCategoryBox = jQuery(this).parents('.home-category-box');
+
+        if ($this.hasClass('edit-task')) {
+            hideShowH4('h4-edit');
+        } else {
+            hideShowH4('h4-add');
+        }
+
+        jQuery.ajax({
+            url: action,
+            method: 'GET'
+        }).done(function (data) {
+            $this.button('reset');
+            if (data.status == 1) {
+                var modal = $('#addNewTask');
+                var content = data.content;
+                jQuery(modal).find('.modal-body').html(content);
+                jQuery(modal).find('.modal-body').find('.field-category-id').val(category);
+                jQuery(modal).find('.action-submit').attr('data-action', action);
+                jQuery(modal).modal('show');
+
+                // http://xdsoft.net/jqplugins/datetimepicker/
+                jQuery('.datepicker').datetimepicker({
+                    format: 'd.m.Y H:i',
+                    lang: 'en'
+                });
+                tinymce.remove();
+                tinymce.init({
+                    selector: '.modal-form textarea',
+                    menubar: false,
+                    setup: function (editor) {
+                        editor.on('change', function () {
+                            editor.save();
+                        });
+                    }
+                });
+            } else if (data.status == 0 && typeof data.message !== 'undefined') {
+                createAlert(data.message, 'danger');
+            } else {
+                createAlert('Error!', 'danger');
+            }
+        });
+        return false;
+    });
+
+    jQuery(el).find('.action-done').click(function () {
+        var action = jQuery(this).attr('data-action');
+        currentCategoryBox = jQuery(this).parents('.home-category-box');
+        jQuery.ajax({
+            url: action,
+            method: 'POST'
+        }).done(function (data) {
+            if (data.status == 1) {
+                // createAlert(data.message);
+                loadTasksBox(currentCategoryBox);
+            } else if (data.status == 0 && typeof data.message !== 'undefined') {
+                createAlert(data.message, 'danger');
+            } else {
+                createAlert('Error!', 'danger');
+            }
+        });
+        return false;
+    });
+
+    jQuery(el).find('.delete-category').click(function () {
+        if (confirm("Are you sure?")) {
+            var action = jQuery(this).attr('data-action');
+
+            jQuery.ajax({
+                url: action,
+                method: 'POST'
+            }).done(function (data) {
+                if (data.status == 1) {
+                    createAlert(data.message);
+                    loadCategories();
+                } else if (data.status == 0 && typeof data.message !== 'undefined') {
+                    createAlert(data.message, 'danger');
+                } else {
+                    createAlert('Error!', 'danger');
+                }
+            });
+        }
+        return false;
+    });
+}
+
+function hideShowH4(action) {
+    if (action != 'h4-add') {
+        jQuery('.h4-edit').show();
+        jQuery('.h4-add').hide();
+    } else {
+        jQuery('.h4-edit').hide();
+        jQuery('.h4-add').show();
+    }
+    var name = jQuery(currentCategoryBox).find('h3 span.name').text();
+    jQuery('.h4-add span, .h4-edit span').text(name);
 }
