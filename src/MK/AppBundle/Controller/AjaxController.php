4a1e4f8296a7cc77df74acee5b2eecaa2d212faf
Marcin Kaźmierski <marcin.ryszard.kazmierski@gmail.com>
2016-11-07 21:53:27 +0100

    Contact form - send mail.

--- src/MK/AppBundle/Controller/AjaxController.php
+++ src/MK/AppBundle/Controller/AjaxController.php
@@ -8,6 +8,8 @@ use MK\AppBundle\Form\ContactType;
 use MK\AppBundle\Repository\TaskRepository;
 use MK\AppBundle\Utils\CategoryTaskPermissions;
 use MK\AppBundle\Utils\TaskPermissions;
+use MK\MailBundle\Service\MailTemplate;
+use MK\UserBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -143,9 +145,33 @@ class AjaxController extends Controller
         if ($form->isSubmitted()) {
             if ($form->isValid()) {
                 $data = $form->getData();
-                // TODO
+                $subject = $data['subject'];
+                /** @var $currentUser User */
+                $currentUser = $this->getUser();
+                $params = array(
+                    'message' => $data['message'],
+                    'author' => $currentUser->getUsername()
+                );
+                /** @var $serviceMail MailTemplate */
+                $serviceMail = $this->get('mk_mail_engine.class');
+                $serviceMail->sendMailWithTemplate($subject, $params, $currentUser->getEmail(), 'contact');
+                // TODO:
                 // http://www.lucas.courot.com/how-to-create-a-contact-form-using-symfony2.html
+
+                $response = array(
+                    'message' => $this->get('translator')->trans('contact.success'),
+                    'status' => 1,
+                    'content' => ''
+                );
+            } else {
+                $response = array(
+                    'message' => $this->get('translator')->trans('contact.invalid_data'),
+                    'status' => 0,
+                    'content' => ''
+                );
             }
+
+            return new JsonResponse($response);
         }
 
         $render = $this->render('MKAppBundle::ajax/contact.html.twig', array(
